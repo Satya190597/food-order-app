5020d2c8814de9faa577c172cac2c9d7484b270d
satya190597 <satyaprakashnandy@gmail.com>
2021-06-30 11:17:06 +0530

    Add modal popup - For cartitem

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,32 @@
 // + Import CSS
 import styles from "./Cart.module.css";
 
+// + Import Component
+import Modal from "../UI/Modal";
+
 const Cart = () => {
   const cartItems = (
     <ul className={styles["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((element) => (
-        <li>{element}</li>
+        <li key={element.id}>{element.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
-      {cartItems}
-      <div className={styles["total"]}>
-        <span>Total Amount</span>
-        <span>35.62</span>
+    <Modal>
+      <div>
+        {cartItems}
+        <div className={styles["total"]}>
+          <span>Total Amount</span>
+          <span>35.62</span>
+        </div>
+        <div className={styles["actions"]}>
+          <button className={styles["button--alt"]}>Close</button>
+          <button className={styles["button"]}>Order</button>
+        </div>
       </div>
-      <div className={styles["actions"]}>
-        <button className={styles["button--alt"]}>Close</button>
-        <button className={styles["button"]}>Order</button>
-      </div>
-    </div>
+    </Modal>
   );
 };
 
